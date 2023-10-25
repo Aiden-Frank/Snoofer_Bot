a12eceeaee24e759427026067739048082a1f66c
Aiden-Frank <aidenjoseph.frank@gmail.com>
2023-10-25 09:25:59 -0700

    Added capability to use gyro for odometry

--- Bot_Code/temp.py
+++ Bot_Code/temp.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-
-from ev3dev2.sensor.lego import UltrasonicSensor
-from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
-from ev3dev2.port import LegoPort
+from ev3dev2.motor import (Motor, OUTPUT_C, OUTPUT_D, MoveDifferential)
+from ev3dev2.wheel import EV3EducationSetTire
 import time
-from smbus import SMBus
-bus=SMBus(1)
+from ev3dev2.sensor.lego import GyroSensor
+from ev3dev2.port import LegoPort
+import math
 
 def set_port(port,mode,device): 
 #   Function to initiate sensors; port should be INPUT_N where N is 1 to 4
@@ -17,10 +16,21 @@ def set_port(port,mode,device):
         sensor.set_device=device
     time.sleep(0.5)
 
-snoofer_port=set_port('pistorms:BBS1',"i2c-thru", "Custom")
-class Snoofer():
-    def __init__(self,bus=SMBus(1)):
-        self.bus=bus
-        self.bus.write_byte_data(0x62, 0x00, 4)
-
-snoofer=Snoofer(bus=bus)
+def print_data():
+    print('Gyro Angle: ',math.radians(gyro.circle_angle()))
+    print('Odometry Angle: ',drive.theta)
+drive=MoveDifferential(OUTPUT_C, OUTPUT_D, EV3EducationSetTire, 152)
+gyroport=set_port(port='pistorms:BAS1', mode='ev3-uart', device='lego-ev3-gyro')
+gyro=GyroSensor('pistorms:BAS1')
+drive.gyro=gyro
+drive.odometry_start(sleep_time=0.04,use_gyro=True)
+time.sleep(1)
+print_data()
+drive.odometry_start(sleep_time=0.04,use_gyro=True)
+drive.turn_degrees(20, 1800, block=True)
+time.sleep(40)
+print()
+drive.off()
+print_data()
+time.sleep(10)
+drive.off()
